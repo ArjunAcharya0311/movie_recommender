df13638974a607c9589399f19032c1d4e028f854
Arjun Acharya <arjun.1acharya@gmail.com>
2020-11-16 17:14:27 -0500

    uploaded dataset extraction code

---
+++ get_data.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+
+df1 = pd.read_csv('tmdb_5000_credits.csv')
+df2 = pd.read_csv('tmdb_5000_movies.csv')
+
+
+df1.columns = ['id','title','cast','crew']
+df2 = df2.merge(df1, on='id')
+
+df2.to_csv("movie_list.csv", index = False)
